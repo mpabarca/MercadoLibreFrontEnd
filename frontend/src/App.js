7ccaf4dc49a35aa80fdf8ef54e37c7682e7b4e55
María Paz Abarca <mariapaz.abarca@toctoc.com>
2020-10-24 18:12:32 -0300

    se agrega sass y archivos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../src/assets/img/logo.svg';
 
 const App = () => {
   const [apiResponse, setApiResponse] = useState('');  
